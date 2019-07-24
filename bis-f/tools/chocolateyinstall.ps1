48038e69aba0a8d7be2752ca384637a1a8ef26fa
Ryan Butler <ryan@techdrabble.com>
2019-07-24 07:53:56 -0500

    version

--- bis-f/tools/chocolateyinstall.ps1
+++ bis-f/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
-  url 			    = 'https://github.com/EUCweb/BIS-F/releases/download/6.1.2/setup-BIS-F-6.1.2_build01.108.exe' 	
+  url 			    = 'https://github.com/EUCweb/BIS-F/releases/download/6.1.2/setup-BIS-F-6.1.2_build01.109.exe' 	
   softwareName  = 'bis-f*'
 
-  checksum      = '98d7d7a38b830944048f972f0a0c53c0726f6080b46ef54ef72a3888d7395105'
+  checksum      = '3368bff518d122312ac7972cf156db7a60d2de7f5962d6830b87a1210296d297'
   checksumType  = 'sha256'
 
   silentArgs    = "/VERYSILENT /NORESTART"
