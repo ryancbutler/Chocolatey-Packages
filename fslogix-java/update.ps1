49f69a70bad2ca03c3a29b6f9325c87cbf805fc6
ryancbutler <ryan@ryancbutler.com>
2019-11-11 09:55:34 -0600

    better url check

--- fslogix-java/update.ps1
+++ fslogix-java/update.ps1
@@ -6,13 +6,19 @@ function global:au_BeforeUpdate {
 
 function global:au_GetLatest {
     $url = "https://aka.ms/fslogix_download"
-    mkdir temp -Force
-    $download_page = Invoke-WebRequest -Uri $url -UseBasicParsing -outfile "temp\fslogix.zip"
-    7z e temp\fslogix.zip -otemp -spf -y
-    $Latest.checksum_zip = Get-FileHash temp\fslogix.zip | % Hash
-    $version = (get-item .\temp\x64\Release\FSLogixAppsJavaRuleEditorSetup.exe).VersionInfo.FileVersion
-    #$Latest.checksum32 = Get-FileHash temp\Win32\Release\FSLogixAppsJavaRuleEditorSetup.exe | % Hash
-    #$Latest.checksum64 = Get-FileHash temp\x64\Release\FSLogixAppsJavaRuleEditorSetup.exe| % Hash
+    #mkdir temp -Force
+    # $download_page = Invoke-WebRequest -Uri $url -UseBasicParsing -outfile "temp\fslogix.zip"
+    # 7z e temp\fslogix.zip -otemp -spf -y
+    # $Latest.checksum_zip = Get-FileHash temp\fslogix.zip | % Hash
+    # $version = (get-item .\temp\x64\Release\FSLogixAppsSetup.exe).VersionInfo.FileVersion
+
+    $uri = Invoke-WebRequest -MaximumRedirection 0 -Uri $url -ErrorAction SilentlyContinue -UseBasicParsing
+    $uri2 = Invoke-WebRequest -MaximumRedirection 0 -Uri $uri.Headers.Location -ErrorAction SilentlyContinue -UseBasicParsing
+    $file = $uri2.Headers.Location -split "/" | Select-Object -Last 1
+    $file -match "\d+(\.\d+)+"
+    $version = $Matches[0]
+    #$Latest.checksum32 = Get-FileHash temp\Win32\Release\FSLogixAppsSetup.exe | % Hash
+    #$Latest.checksum64 = Get-FileHash temp\x64\Release\FSLogixAppsSetup.exe | % Hash
     #$string = $url -split "/" | select -Last 1 -Skip 1
     #$version = $string.Remove(($string.LastIndexOf(".")),"1")
     #write-host $version
@@ -30,4 +36,4 @@ function global:au_SearchReplace {
 }
 
 update -ChecksumFor none -NoCheckUrl
-remove-item -Path "temp" -Force -Recurse
+#remove-item -Path "temp" -Force -Recurse
