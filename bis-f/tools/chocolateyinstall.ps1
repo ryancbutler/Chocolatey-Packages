7c69e9e0d8ebd2957d472f26724506d2b6ed839d
ryancbutler <ryancbutler@users.noreply.github.com>
2020-06-15 08:15:15 -0500

    update

--- bis-f/tools/chocolateyinstall.ps1
+++ bis-f/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
-  url 			    = 'https://github.com/EUCweb/BIS-F/releases/download/7.1912.2/setup-BIS-F-7.1912.2.11029.exe' 	
+  url 			    = 'https://github.com/EUCweb/BIS-F/releases/download/7.1912.3/setup-BIS-F-7.1912.3.11036.exe' 	
   softwareName  = 'bis-f*'
 
-  checksum      = '35d1251bd7fbf03dc2ccfdac874db7e862ae352c74dae9430e6a1ae77bdbc7f7'
+  checksum      = 'ee3e60afa6b526b08a21c56304af1a3623d7a28f713463bca2ca7e1e11ca0783'
   checksumType  = 'sha256'
 
   silentArgs    = "/qn /norestart"
