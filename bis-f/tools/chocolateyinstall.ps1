c4ca19eed578b6d7b03487b19ce07be6d1681ce4
RyanCButler <ryan@ryancbutler.com>
2020-09-16 12:01:41 +0000

    AU: 1 updated - bis-f [skip ci] https://gist.github.com/7b5093c146bccff82f333d93e4a2e65a/96bb062ef87c4673165ccb73036ad1d2e826e815

--- bis-f/tools/chocolateyinstall.ps1
+++ bis-f/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'MSI'
-  url 			    = 'https://github.com/EUCweb/BIS-F/releases/download/7.1912.4/setup-BIS-F-7.1912.4.11038.msi' 	
+  url 			    = 'https://github.com/EUCweb/BIS-F/releases/download/7.1912.5/setup-BIS-F-7.1912.5.11040.MSI' 	
   softwareName  = 'bis-f*'
 
-  checksum      = '0213cf731f33266a4b94fec1bdeb74d801cd80c9cdcaee7ad679f6f7d145c33e'
+  checksum      = '2708b001d1cdec30c86517a9c6d55e1b0876985d24ec2640d93145fd52faf3c0'
   checksumType  = 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`""
