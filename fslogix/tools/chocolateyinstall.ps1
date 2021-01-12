a4b130cdcd29a78d1dd32105f6b360de89a5f115
Ryan Butler <ryancbutler@users.noreply.github.com>
2021-01-12 14:52:50 -0600

    adds

--- fslogix/tools/chocolateyinstall.ps1
+++ fslogix/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $zipargs = @{
   packageName  = $env:ChocolateyPackageName
   fileFullPath = $toolsdir
   url = 'https://download.microsoft.com/download/4/8/2/4828e1c7-176a-45bf-bc6b-cce0f54ce04c/FSLogix_Apps_2.9.7654.46150.zip'
-  checksum = '945E5E9ABC4CD6760ABCA7002800FD64394B22C4A465C52EEFAF2249B7B0FB28'
+  checksum = '184CB679AB9C1440428405F3F709A526E687D9820543A166055DBABCC864197D'
   checksumtype = "sha256"
 }
 
