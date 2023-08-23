f2512e0b8509524e1b03fe71276cf4cac32fbeb0
RyanCButler <ryan@ryancbutler.com>
2023-08-23 12:02:22 +0000

    AU: 3 updated - fslogix fslogix-java fslogix-rule [skip ci] https://gist.github.com/ryancbutler/7b5093c146bccff82f333d93e4a2e65a/8508b58739e17edfb67d83004bbbcbacbcf72384

--- fslogix-rule/tools/chocolateyinstall.ps1
+++ fslogix-rule/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@
 $zipargs = @{
   packageName  = $env:ChocolateyPackageName
   fileFullPath = $toolsdir
-  url          = 'https://download.microsoft.com/download/c/4/4/c44313c5-f04a-4034-8a22-967481b23975/FSLogix_Apps_2.9.8440.42104.zip'
-  checksum     = 'F6273EC30862815EBB0CA2E634A24972BF86A042ED297A8F17F0F65A541E335B'
+  url          = 'https://download.microsoft.com/download/9/7/b/97b4c64b-ffc9-447c-b39e-3afba4672ee8/FSLogix_Apps_2.9.8612.60056.zip'
+  checksum     = 'E13808071BACD73FB47E37DA35E0EB53B1562B7E11A944BDC9C6FE55BD19051F'
   checksumtype = "sha256"
 }
 
